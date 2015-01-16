f0754185ee9430fe4c3491b976b0cb6272f05e84
kbooras <kbooras@ufl.edu>
2015-01-15 19:21:05 -0500

    Changed first screen shown by adding sign in and register buttons. Added activity for registering. Still need to implement logic for this.

---
+++ IntegratingFacebookTutorial/src/com/example/kirstiebooras/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.kirstiebooras;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.parse.integratingfacebooktutorial.R;
+
+/**
+ * Created by kirstiebooras on 1/15/15.
+ */
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.register_activity);
+
+    }
+
+    public void onRegisterClick(View v) {
+
+    }
+}
