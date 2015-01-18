8da1abfa1a52eeb9e4f94e566295636dadf52b31
kbooras <kbooras@ufl.edu>
2015-01-18 16:59:00 -0500

    Added forgot password functionality to SigninActivity

--- IntegratingFacebookTutorial/src/com/example/kirstiebooras/SigninActivity.java
+++ IntegratingFacebookTutorial/src/com/example/kirstiebooras/SigninActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
 import com.parse.integratingfacebooktutorial.R;
 
 /**
@@ -28,6 +29,8 @@ public class SigninActivity extends Activity {
     public EditText password;
     public String emailTxt;
     public String passwordTxt;
+    public final int INVALID_EMAIL = 125;
+    public final int EMAIL_NOT_FOUND = 205;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,5 +74,61 @@ public class SigninActivity extends Activity {
                 }
             });
         }
+    }
+
+    public void onForgotPasswordClick(View v) {
+        final EditText resetPasswordEmail = new EditText(this);
+        new AlertDialog.Builder(this)
+                .setTitle("Forgot Password")
+                .setMessage("Enter the email associated with your account")
+                .setView(resetPasswordEmail)
+                .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String resetPasswordEmailTxt = resetPasswordEmail.getText().toString();
+                        ParseUser.requestPasswordResetInBackground(resetPasswordEmailTxt,
+                                new RequestPasswordResetCallback() {
+                                    public void done(ParseException e) {
+                                        if (e == null) {
+                                            // An email was successfully sent with reset
+                                            // instructions.
+                                            Toast.makeText(getApplicationContext(),
+                                                    "Reset email sent!",
+                                                    Toast.LENGTH_LONG);
+                                            Log.v(TAG, "reset email sent!");
+                                        } else {
+                                            // Something went wrong. Look at the ParseException
+                                            // to see what's up.
+                                            Log.v(TAG, String.valueOf(e.getCode()));
+                                            displayResetErrorMessage(e.getCode());
+                                        }
+                                    }
+                                });
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Do nothing.
+                    }
+                }).show();
+    }
+
+    public void displayResetErrorMessage(int errorCode) {
+        String message;
+        if (errorCode == INVALID_EMAIL) {
+            message = "The email you entered was invalid";
+        } else if (errorCode == EMAIL_NOT_FOUND) {
+            message = "The email you entered is not associated with an account";
+        } else {
+            message = "The reset password failed. Please try again later.";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle("Reset Password Failed")
+                .setMessage(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Do nothing.
+                    }
+                }).show();
     }
 }
