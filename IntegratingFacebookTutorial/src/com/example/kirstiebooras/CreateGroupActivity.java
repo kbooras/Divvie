81b0e1dbf7a9c8cae1c1d33ca2fb5b34887bd7d0
kbooras <kbooras@ufl.edu>
2015-01-25 21:34:51 -0500

    Modularized CreateGroupActivity. Renamed member variables to Android convention

--- IntegratingFacebookTutorial/src/com/example/kirstiebooras/CreateGroupActivity.java
+++ IntegratingFacebookTutorial/src/com/example/kirstiebooras/CreateGroupActivity.java
@@ -36,21 +36,20 @@ import java.util.List;
  */
 public class CreateGroupActivity extends Activity {
 
-    private LinearLayout layout;
-    private EditText groupName;
-    private int editTextCount;
-    private int editTextWidth;
-    private int editTextMarginTop;
-    private int editTextPaddingLeft;
-    private int buttonSize;
-    private static Resources res;
+    private LinearLayout mLayout;
+    private EditText mGroupName;
+    private int mEditTextCount;
+    private int mEditTextWidth;
+    private int mEditTextMarginTop;
+    private int mButtonSize;
+    private List<EditText> mAllEditTexts = new ArrayList<EditText>();
+    private static Resources mResources;
     private static final float EDIT_TEXT_WIDTH_DP = 328.0f;
     private static final float EDIT_TEXT_MARGIN_TOP_DP = 11.0f;
     private static final float BUTTON_SIZE_DP = 35.0f;
     private static final String TAG = "CreateGroupActivity";
     private static final int EDIT_TEXT_ID_CONSTANT = 1000;
     private static final int BUTTON_ID_CONSTANT = 2000;
-    private List<EditText> allEditTexts = new ArrayList<EditText>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,18 +59,18 @@ public class CreateGroupActivity extends Activity {
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        res = getResources();
+        mResources = getResources();
 
         final float scale = getResources().getDisplayMetrics().density;
-        editTextWidth = (int) (EDIT_TEXT_WIDTH_DP * scale + 0.5f);
-        editTextMarginTop = (int) (EDIT_TEXT_MARGIN_TOP_DP * scale + 0.5f);
-        buttonSize = (int) (BUTTON_SIZE_DP * scale + 0.5f);
+        mEditTextWidth = (int) (EDIT_TEXT_WIDTH_DP * scale + 0.5f);
+        mEditTextMarginTop = (int) (EDIT_TEXT_MARGIN_TOP_DP * scale + 0.5f);
+        mButtonSize = (int) (BUTTON_SIZE_DP * scale + 0.5f);
 
-        layout = (LinearLayout) findViewById(R.id.layout);
-        groupName = (EditText) findViewById(R.id.groupName);
-        allEditTexts.add((EditText) findViewById(R.id.email1));
+        mLayout = (LinearLayout) findViewById(R.id.layout);
+        mGroupName = (EditText) findViewById(R.id.groupName);
+        mAllEditTexts.add((EditText) findViewById(R.id.email1));
 
-        editTextCount = 1;
+        mEditTextCount = 1;
     }
 
     @Override
@@ -101,66 +100,80 @@ public class CreateGroupActivity extends Activity {
     public void onAddEditTextClick(View v) {
         // Dynamically adds a new EditText below the last EditText.
         // +1 because starts at index 0 and the TextView is above all EditTexts
-        layout.addView(createNewEmailView(), editTextCount + 1);
+        mLayout.addView(createNewEmailView(), mEditTextCount + 1);
     }
 
     private FrameLayout createNewEmailView() {
         // Create FrameLayout which holds EditText and delete button
         final FrameLayout frame = new FrameLayout(this);
         final LinearLayout.LayoutParams frameParams = new LinearLayout.LayoutParams(
-                editTextWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
-        frameParams.setMargins(0, editTextMarginTop, 0, 0);
+                mEditTextWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
+        frameParams.setMargins(0, mEditTextMarginTop, 0, 0);
         frameParams.gravity = Gravity.CENTER_HORIZONTAL;
         frame.setLayoutParams(frameParams);
 
+        // Create the EditText
+        frame.addView(createEditText());
 
-        //Create the EditText
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+        // Create the delete button
+        frame.addView(createDeleteButton());
+
+        // Hide delete button from previous EditText
+
+        return frame;
+    }
+
+    private EditText createEditText() {
+        final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         final EditText editText = new EditText(this);
-        editText.setId(EDIT_TEXT_ID_CONSTANT + editTextCount);
+
+        Log.v(TAG, "Created EditText " + mEditTextCount);
+        mEditTextCount++;
+
+        editText.setId(EDIT_TEXT_ID_CONSTANT + mEditTextCount);
         editText.setLayoutParams(params);
 
         int paddingLeft = editText.getPaddingLeft();
         int paddingBottom = editText.getPaddingBottom();
-        editText.setPadding(paddingLeft, 0, buttonSize, paddingBottom);
+        editText.setPadding(paddingLeft, 0, mButtonSize, paddingBottom);
         editText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-        editText.setHint(String.format(res.getString(R.string.create_group_email), editTextCount+1));
-        allEditTexts.add(editText);
-        frame.addView(editText);
+        editText.setHint(String.format(mResources.getString(R.string.create_group_email),
+                mEditTextCount));
+        mAllEditTexts.add(editText);
 
+        return editText;
+    }
 
-        // Create the delete button
-        params = new FrameLayout.LayoutParams(buttonSize, buttonSize, Gravity.RIGHT);
+    private Button createDeleteButton() {
+        final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(mButtonSize,
+                mButtonSize, Gravity.RIGHT);
         final Button button = new Button(this);
-        button.setId(BUTTON_ID_CONSTANT + editTextCount);
+        button.setId(BUTTON_ID_CONSTANT + mEditTextCount);
         button.setLayoutParams(params);
         button.setBackgroundResource(R.drawable.edittext_delete_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("button", "clicked " + view.getId());
+                Log.v(TAG, "Clicked delete EditText" + view.getId());
+                // Remove the EditText and add delete button to previous EditText
             }
         });
-        frame.addView(button);
 
-        Log.v(TAG, "Created EditText " + editTextCount);
-        editTextCount++;
-
-        return frame;
+        return button;
     }
 
     public void onCreateGroupClick(View v) {
-        String groupNameTxt = groupName.getText().toString();
+        String groupNameTxt = mGroupName.getText().toString();
 
         // Create an array of all emails added in the EditTexts + the current user.
-        String[] memberEmails = new String[allEditTexts.size() + 1];
+        String[] memberEmails = new String[mAllEditTexts.size() + 1];
         memberEmails[0] = ParseUser.getCurrentUser().getEmail();
 
         // Check that each email is valid and add to the array of members
         String email;
-        for(int i=0; i < allEditTexts.size(); i++){
-            email = allEditTexts.get(i).getText().toString();
+        for(int i=0; i < mAllEditTexts.size(); i++){
+            email = mAllEditTexts.get(i).getText().toString();
             if (!isValidEmail(email)){
                 // Display invalid email message
                 displayInvalidEmailMessage(i+1);
@@ -204,35 +217,38 @@ public class CreateGroupActivity extends Activity {
 
     private void displayInvalidEmailMessage (int emailNumber) {
         new AlertDialog.Builder(this)
-                .setTitle(res.getString(R.string.group_invalid_email_alert_title))
-                .setMessage(String.format(res.getString(R.string.group_invalid_email_alert_message),
-                        emailNumber))
-                .setPositiveButton(res.getString(R.string.ok), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Do nothing.
-                    }
+                .setTitle(mResources.getString(R.string.group_invalid_email_alert_title))
+                .setMessage(String.format(mResources.getString(
+                        R.string.group_invalid_email_alert_message), emailNumber))
+                .setPositiveButton(mResources.getString(R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Do nothing.
+                            }
                 }).show();
     }
 
     private void displayCreateGroupFailedMessage () {
         new AlertDialog.Builder(this)
-                .setTitle(res.getString(R.string.create_group_failed_alert_title))
-                .setMessage(res.getString(R.string.create_group_failed_alert_message))
-                .setPositiveButton(res.getString(R.string.ok), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Do nothing.
-                    }
+                .setTitle(mResources.getString(R.string.create_group_failed_alert_title))
+                .setMessage(mResources.getString(R.string.create_group_failed_alert_message))
+                .setPositiveButton(mResources.getString(R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Do nothing.
+                            }
                 }).show();
     }
 
     private void displayDuplicateEmailMessage() {
         new AlertDialog.Builder(this)
-                .setTitle(res.getString(R.string.duplicate_email_alert_title))
-                .setMessage(res.getString(R.string.duplicate_email_alert_message))
-                .setPositiveButton(res.getString(R.string.ok), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Do nothing.
-                    }
+                .setTitle(mResources.getString(R.string.duplicate_email_alert_title))
+                .setMessage(mResources.getString(R.string.duplicate_email_alert_message))
+                .setPositiveButton(mResources.getString(R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Do nothing.
+                            }
                 }).show();
     }
 }
