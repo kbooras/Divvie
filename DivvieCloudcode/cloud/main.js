e870026f666ea4c73d011fb7c1944ec4043207af
kbooras <kbooras@ufl.edu>
2015-02-04 17:00:59 -0800

    Groups fragment: Display name if they have an account, email otherwise

--- DivvieCloudcode/cloud/main.js
+++ DivvieCloudcode/cloud/main.js
@@ -1,30 +1,56 @@
 
 Parse.Cloud.define("sendChargeEmail", function(request, response) {
-  var toEmail = request.params.toEmail;
-  var toName = request.params.toName;
-  var fromName = request.params.fromName;
-  var groupName = request.params.groupName;
-  var chargeDescription = request.params.chargeDescription;
-  var amount = request.params.amount;
+    var toEmail = request.params.toEmail;
+    var toName = request.params.toName;
+    var fromName = request.params.fromName;
+    var groupName = request.params.groupName;
+    var chargeDescription = request.params.chargeDescription;
+    var amount = request.params.amount;
 
-  var Mandrill = require('mandrill');
-  Mandrill.initialize('M5f77U0mhIWJxzMWFDhBkw');
-  Mandrill.sendEmail({
-    message: {
-      text: "You have been charged " + amount + ".",
-      subject: fromName + " has charged your group " + groupName + " for " + chargeDescription + ".",
-      from_email: "divvie-no-reply@parseapps.com",
-      from_name: "Divvie App",
-      to: [
-        {
-          email: toEmail,
-          name: toName
-        }
-      ]
-    },
-    async: true
-  }, {
-    success: function(httpResponse) { response.success("Email sent!"); },
-    error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
-  });
+    var Mandrill = require('mandrill');
+    Mandrill.initialize('M5f77U0mhIWJxzMWFDhBkw');
+    Mandrill.sendEmail({
+        message: {
+        text: "You have been charged " + amount + ".",
+        subject: fromName + " has charged your group " + groupName + " for " + chargeDescription + ".",
+        from_email: "divvie-no-reply@parseapps.com",
+        from_name: "Divvie App",
+        to: [{
+                email: toEmail,
+                name: toName
+            }]
+        },
+        async: true
+    }, {
+        success: function(httpResponse) { response.success("Email sent!"); },
+        error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
+    });
 });
+
+/*Parse.Cloud.define("getMemberNames", function(request, response) {
+    var members = request.params.members;
+    var membersWithNames = new Array(members.length);
+
+    for (var i = 0; i < members.length; i++) {
+        Parse.Cloud.useMasterKey();
+        var query = new Parse.Query(Parse.User);
+        console.log("member to find: " + members[i]);
+        query.equalTo("email", members[i]);
+        query.find({
+            success: function(results) {
+                if (results.length == 0) {
+                    console.log("Empty results");
+                    membersWithNames[i] = members[i];
+                } else {
+                    membersWithNames[i] = results[0].get("fullName");
+                    console.log("Empty results");
+                }
+            },
+            error: function() {
+              response.error("Query failed. Error = " + error.message);
+            }
+          });
+    }
+
+    response.success(membersWithNames);
+});*/
--- IntegratingFacebookTutorial/src/com/example/kirstiebooras/GroupsAdapter.java
+++ IntegratingFacebookTutorial/src/com/example/kirstiebooras/GroupsAdapter.java
@@ -10,10 +10,15 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.integratingfacebooktutorial.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Custom ArrayAdapter to load a user's group info into the groups fragment.
@@ -59,14 +64,27 @@ public class GroupsAdapter extends ArrayAdapter<ParseObject> {
         return rowView;
     }
 
-    private LinearLayout createMemberView(String member, int textViewNumber) {
+    private LinearLayout createMemberView(final String memberEmail, int textViewNumber) {
         LinearLayout memberView = (LinearLayout) View.inflate(mContext, R.layout.group_member_row,
                 null);
-        TextView textView = (TextView) memberView.findViewById(R.id.member);
+        final TextView textView = (TextView) memberView.findViewById(R.id.member);
         textView.setId(textViewNumber);
-        textView.setText(member);
 
-        //TODO display name if they have an account, email otherwise
+        // Display name if they have an account, email otherwise
+        ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
+        userQuery.whereEqualTo("email", memberEmail);
+        userQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> results, ParseException e) {
+                if (results.size() == 0) {
+                    textView.setText(memberEmail);
+                } else {
+                    textView.setText(results.get(0).getString("fullName"));
+                }
+
+            }
+        });
+
 
         return memberView;
     }
