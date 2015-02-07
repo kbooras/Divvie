77d91268fcbfac17fa83e3d5e7a09bf130c44d6b
kbooras <kbooras@ufl.edu>
2015-02-06 23:48:36 -0800

    keys

---
+++ IntegratingFacebookTutorial/src/com/example/kirstiebooras/DivvieApplication.java
@@ -0,0 +1,24 @@
+package com.example.kirstiebooras;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.integratingfacebooktutorial.R;
+
+/**
+ * Created by kirstiebooras on 2/6/15.
+ */
+public class DivvieApplication extends Application {
+
+    public static final String TAG = "DivvieApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this,
+                getString(R.string.PARSE_APPLICATION_ID),
+                getString(R.string.PARSE_CLIENT_ID)
+        );
+    }
+}
